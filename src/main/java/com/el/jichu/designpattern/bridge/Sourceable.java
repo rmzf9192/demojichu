f4684ec94ed27bbaf246b9578659529da5410ac4
tom_glb <goodMorning_pro@163.com>
2018-12-28 11:40:15 +0800

    java设计模式-桥接模式，组合模式，外观模式，享元模式，代理模式，策略模式

---
+++ src/main/java/com/el/jichu/designpattern/bridge/Sourceable.java
@@ -0,0 +1,12 @@
+package com.el.jichu.designpattern.bridge;
+
+/**
+ * @Auther: roman.zhang
+ * @Date: 2018/12/27 20:55
+ * @Version:V1.0
+ * @Description:Sourceable：桥接模式接口
+ *
+ */
+public interface Sourceable {
+    public void method();
+}
---
+++ src/main/java/com/el/jichu/designpattern/strategy/Calculator.java
@@ -0,0 +1,8 @@
+package com.el.jichu.designpattern.strategy;
+
+/**
+ * 同一接口
+ */
+public interface Calculator {
+    public int calculate(String exp);
+}
