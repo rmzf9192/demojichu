a5a1eefcf57389df5ddbf2e74aa83f22a9029c81
tom_glb <goodMorning_pro@163.com>
2019-01-04 16:02:24 +0800

    java 反射

---
+++ src/main/java/com/el/jichu/reflex/TestReflex.java
@@ -0,0 +1,32 @@
+package com.el.jichu.reflex;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @Auther: roman.zhang
+ * @Date: 2019/1/4 15:11
+ * @Version:V1.0
+ * @Description:TestReflex
+ * 反射就是在运行时才知道要操作的类是什么，并且可以在运行时获取类的完整构造，并调用对应的方法。
+ */
+
+public class TestReflex {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Person xiao = new Person();
+        xiao.setAge(21);
+        xiao.setName("jj");
+        System.out.println("person:"+xiao);
+        //使用jdk提供的反射的API
+        Class aClass = Class.forName("com.el.jichu.reflex.Person");
+        Method method = aClass.getMethod("setName", String.class);
+        Method method1 = aClass.getMethod("setAge", Integer.class);
+        Constructor constructor = aClass.getConstructor();
+        Object instance = constructor.newInstance();
+         method.invoke(instance, "lisi");
+         method1.invoke(instance,23);
+        System.out.println("Person:"+instance);
+    }
+}
+
