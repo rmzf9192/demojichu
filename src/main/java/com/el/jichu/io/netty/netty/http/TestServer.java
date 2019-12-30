f2f223b0444367d9c9e4c1cb5c4a125a8c15c018
roman.zhang <1187455023@qq.com>
2019-12-30 17:59:53 +0800

    Netty 入门- http实例编写，unpooled编写

---
+++ src/main/java/com/el/jichu/io/netty/netty/http/TestServer.java
@@ -0,0 +1,33 @@
+package com.el.jichu.io.netty.netty.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author roman zhangfei
+ * @Date 2019/12/30 16:38
+ * @Version V1.0
+ */
+public class TestServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind("127.0.0.1",6668).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
