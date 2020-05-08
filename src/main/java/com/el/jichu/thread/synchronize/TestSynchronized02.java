1ca0d5233090d8a16e67e8c1efdf5eb0e62557f3
1187455023@qq.com <148697zf>
2020-05-08 08:28:21 +0800

    synchronized对象锁与类锁

---
+++ src/main/java/com/el/jichu/thread/synchronize/TestSynchronized02.java
@@ -0,0 +1,32 @@
+package com.el.jichu.thread.synchronize;
+
+/**
+ * @author Roman.Zhang
+ * @date 2020/5/7
+ * @description:
+ */
+public class TestSynchronized02 {
+    public static synchronized void minus() {
+        int count = 5;
+        for (int i = 0; i < 5; i++) {
+            count--;
+            System.out.println(Thread.currentThread().getName() + " - " + count);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    public synchronized void minus2() {
+        int count = 5;
+        for (int i = 0; i < 5; i++) {
+            count--;
+            System.out.println(Thread.currentThread().getName() + " - " + count);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
